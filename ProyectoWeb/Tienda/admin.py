d9f268897c5efcdfb8f0307f2b3e78d8f3a7dc6f
Sebastian01 <sebastiansal24@gmail.com>
2023-06-27 14:54:45 -0500

    Apps anteriores terminadas, los problemas en la base de datos fueron solucionados correctamente

--- ProyectoWeb/Tienda/admin.py
+++ ProyectoWeb/Tienda/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Categorias, Producto
 
 class Admincategorias(admin.ModelAdmin):
-    reandoly_fields=('create','update')
+    reandoly_fields=('create','updated')
     
 class Adminproducto(admin.ModelAdmin):
-    reandoly_fields=('create','update')
+    reandoly_fields=('create','updated')
 
 
 admin.site.register(Categorias,Admincategorias)
--- ProyectoWeb/Tienda/models.py
+++ ProyectoWeb/Tienda/models.py
@@ -3,16 +3,16 @@ from django.db import models
 
 class Categorias(models.Model):
     nombre=models.CharField(max_length=50)
-    create=models.DateTimeField(auto_now_add=True)
-    update=models.DateTimeField(auto_now_add=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.nombre
     
 class Producto(models.Model):
     nombre=models.CharField(max_length=50)
-    create=models.DateTimeField(auto_now_add=True)
-    update=models.DateTimeField(auto_now_add=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
     categoria=models.ForeignKey(Categorias, on_delete=models.CASCADE)
     precio=models.FloatField()
     imagen=models.ImageField(upload_to='Tienda', null=True,blank=True)
